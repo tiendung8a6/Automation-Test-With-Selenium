04634a0ec5d500183a73b1bc2e7e74cfa727163f
tiendung8a6 <tiendung8a6@gmail.com>
2024-01-19 19:22:17 +0700

    BAI 2: Tim UCLN & BCNN

---
+++ Lab1/UNB/src/App.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class App {
+    public static int nhap() {
+        Scanner input = new Scanner(System.in);
+        boolean check = false;
+        int n = 0;
+        while (!check) {
+            System.out.print(" ");
+            try {
+                n = input.nextInt();
+                check = true;
+            } catch (Exception e) {
+                System.out.println("Ban phai nhap so! hay nhap lai...");
+                input.nextLine();
+            }
+        }
+        return (n);
+    }
+
+    public static int UCLN(int a, int b) {
+        while (a != b) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+        return (a);
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Nhap a: ");
+        int a = nhap();
+        System.out.println("Nhap b: ");
+        int b = nhap();
+        System.out.println("Uoc chung lon nhat cua " + a + " va " + b + " la " + UCLN(a, b));
+        System.out.println("Boi chung lon nhat cua " + a + " va " + b + " la " + ((a * b) / UCLN(a, b)));
+    }
+}
